6206d2dbeebae373b3e33d261b8e7cb803c6a846
jesus <jealoma_1997@hotmail.com>
2023-04-12 20:10:01 -0300

    ultima version

---
+++ app/app_contacto.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('enviar_form');
+form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        alert ("ahora eres una suculenta")
+        })
