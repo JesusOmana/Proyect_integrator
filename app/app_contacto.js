ed37fb413bd7e8b931642adaeb5bab82de6d2ce2
jesus <jealoma_1997@hotmail.com>
2023-04-12 21:03:14 -0300

    pagina contacto finalizado

--- app/app_contacto.js
+++ app/app_contacto.js
@@ -1,5 +1,11 @@
 const form = document.getElementById('enviar_form');
 form.addEventListener('submit', (event) => {
         event.preventDefault();
-        alert ("ahora eres una suculenta")
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'ahora eres suculento',
+            showConfirmButton: false,
+            timer: 1500
+          })
         })
